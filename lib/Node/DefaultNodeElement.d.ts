a6ee019d48692b43d6766a950c6dab13f514befd
Chad Golob <chad.golob@gmail.com>
2022-09-12 18:57:42 -0700

    Include lib directory

---
+++ lib/Node/DefaultNodeElement.d.ts
@@ -0,0 +1,6 @@
+import React from 'react';
+import { CustomNodeElementProps } from '../types/common';
+export interface DefaultNodeElementProps extends CustomNodeElementProps {
+}
+declare const DefaultNodeElement: React.FunctionComponent<DefaultNodeElementProps>;
+export default DefaultNodeElement;
